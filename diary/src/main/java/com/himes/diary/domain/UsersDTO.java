0a64f2d6b90435777f6079721ed902aad739f1b9
duck_ach <djagmlfk@naver.com>
2023-04-02 15:35:11 +0900

    sql 데이터타입수정, userDTO

--- diary/src/main/java/com/himes/diary/domain/UsersDTO.java
+++ diary/src/main/java/com/himes/diary/domain/UsersDTO.java
@@ -1,5 +1,34 @@
 package com.himes.diary.domain;
 
-public class UsersDTO {
+import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersDTO {
+	
+	private int userNo;
+	private String userId;
+	private String UserPw;
+	private String name;
+	private String nName;
+	private String gender;
+	private String email;
+	private String mobile;
+	private String birthday;
+	private int agreeCode;
+	private String snsType;
+	private Date joinDate;
+	private String ssId;
+	private Date ssLimitDate;
+	private String ip;
+	private Date pwModifyDate;
+	
+	
 }
