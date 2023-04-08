3c1c3f2b7d44ed11b131575f5d704ab7fa9482a4
jje0ngyu <naniaerang@naver.com>
2023-04-08 12:25:19 +0900

    mybatis 수정

--- diary/src/main/java/com/himes/diary/service/DailyServiceImpl.java
+++ diary/src/main/java/com/himes/diary/service/DailyServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.himes.diary.domain.DailyDTO;
